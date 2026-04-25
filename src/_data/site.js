bf956679f4cdcd20c0c4ce1c3e6ff2c134e0246d
RyanWheeler7321 <63214391+RyanWheeler7321@users.noreply.github.com>
2026-04-25 11:31:11 -0400

    Refine tool icons and update columns layout

--- src/_data/site.js
+++ src/_data/site.js
@@ -4,6 +4,7 @@ module.exports = {
   description: "Ryan Wheeler",
   url: "https://r7321.art",
   author: "Ryan Wheeler",
+  assetVersion: "2026-04-25",
   navigation: [
     { label: "Updates", href: "/updates/" },
     { label: "Projects", href: "/projects/" },
